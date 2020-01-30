e983bf7363f53cd46404c2815148ef70b1e562eb
tankov <tan.kov@hotmail.com>
2020-01-30 16:22:06 +0300

    add content, include bootstrap,addd navbar, deploy on heroku

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Page Title')
+
+@section('content')
+    <h2>This is my body content.</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+//$router->get('/', function () use ($router) {
+    //return $router->app->version();
+//});
+
+$router->get(
+    '/', function () {
+        return view('index', []);
+    }
+);
